46ec983f49dac6727e2ef5e0223aa5dffd62fcc9
Rachel Keslensky <rach@lid.cc>
2015-12-15 13:05:00 -0800

    feat: made kitty clicker work like described in course
    ... okay, so I mostly "referenced" it from the course materials
    https://github.com/udacity/ud989-cat-clicker-premium-vanilla/blob/master/js/app.js
    but it wasn't making sense any other way and I really just needed
    to get it done. Even in doing that I had to debug it the hard way
    because it was still throwing errors at me, mostly to do with the
    renaming of various variables from "cat" to "kitty" to make it make
    more sense to me.

--- js/main.js
+++ js/main.js
@@ -1,69 +1,109 @@
 // referenced from https://github.com/udacity/ud989-pizzamvo/blob/master/js/app.js
-
-$(function() {
-
-	var data = {
-		lastID: 0;
-		kitties: []
-	};
-
-	var octopus = {
-		addKitty: function() {
-			var thisID = ++data.lastID;
-
-			data.kitties.push({
-				id. thisID,
-				visible: true
-			});
-			view.render();
+var data = {
+	currentKitty: null,
+	kitties: [
+		{
+			name: "Mipsy",
+			clickCount: 0,
+			img: "img/cat1.jpg"
 		},
-
-		removeKitty: function(kitty){
-			var clickedKitty = data.kitties[ kitty.id - 1 ]
-			clickedKitty.visible = false;
-			view.render();
+		{
+			name: "Blinky",
+			clickCount: 0,
+			img: "img/cat2.jpg"
 		},
-
-		getVisibleKitties: function() {
-			var visibleKitties = data.kitties.filter(function(kitty) {
-				return kitty.visible;
-			});
-			return visibleKitties;
+		{
+			name: "Boopsy",
+			clickCount: 0,
+			img: "img/cat3.jpg"
 		},
+		{
+			name: "Slinky",
+			clickCount: 0,
+			img: "img/cat4.jpg"
+		},
+		{
+			name: "Zazzles",
+			clickCount: 0,
+			img: "img/cat5.jpg"
+		},
+	]
+};
 
-		init: function() {
-			view.init();
+var octopus = {
+	init: function() {
+		data.currentKitty = data.kitties[0];
+		kittyListView.init();
+		kittyView.init();
+	},
+
+	getCurrentKitty: function() {
+		return data.currentKitty;
+	},
+
+	getKitties: function() {
+		return data.kitties;
+	},
+
+	setCurrentKitty: function(kitty) {
+		data.currentKitty = kitty;
+	},
+
+	incrementCounter: function () {
+		data.currentKitty.clickCount++;
+		kittyView.render();
+	}
+};
+
+var kittyView = {
+	init: function() {
+		this.kittyElem = document.getElementById('kitty');
+		this.kittyNameElem = document.getElementById('kitty-name');
+		this.kittyImageElem = document.getElementById('kitty-img');
+		this.countElem = document.getElementById('kitty-count');
+
+		this.kittyImageElem.addEventListener('click', function(){
+			octopus.incrementCounter();
+		});
+
+		this.render();
+	},
+
+	render: function() {
+		var currentKitty = octopus.getCurrentKitty();
+		this.countElem.textContent = currentKitty.clickCount;
+		this.kittyNameElem.textContent = currentKitty.name;
+		this.kittyImageElem.src = currentKitty.img;
+	}
+};
+
+var kittyListView = {
+	init: function() {
+		this.kittyListElem = document.getElementById('kitty-list');
+		this.render();
+	},
+
+	render: function() {
+		var kitty, elem, i;
+		var kitties = octopus.getKitties();
+
+		this.kittyListElem.innerHTML='';
+
+		for (i = 0; i < kitties.length; i++) {
+			kitty = kitties[i];
+
+			elem = document.createElement('li');
+			elem.textContent = kitty.name;
+
+			elem.addEventListener('click', (function(kittyCopy) {
+				return function() {
+					octopus.setCurrentKitty(kittyCopy);
+					kittyView.render();
+				};
+			})(kitty));
+
+			this.kittyListElem.appendChild(elem);
 		}
-	};
-
-	var view = {
-		init: function() {
-			var addKittyButton = $('.add-kitty');
-			addKittyButton.click(function() {
-				octopus.addKitty();
-			});
-
-			this.$kittyList = $('.kitty-list');
-			this.kittyTemplate = $('script[data-template="kitty"]').html();
-
-			this.$kittyList.on('click', '.remove-kitty', function(e) {
-				var kitty = $(this).parents('.kitty').data();
-				octopus.removeKitty(kitty);
-				return false;
-			});
-		},
-
-		render: function() {
-			var $kittyList = this.$kittyList,
-				kittyTemplate = this.kittyTemplate;
-
-			$kittyList.html('');
-			octopus.getVisibleKitties().forEach(function(kitty) {
-				var thisTemplate = kittyTemplate.replace(/{{id}}/g, kitty.id);
-				$kittyList.append(thisTemplate);
-			});
-		}
-	};
-
-	octopus.init();
-}());
+	}
+}
+octopus.init();
